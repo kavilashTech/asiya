0843ec59db5a3e53050372a74879cd84a060d218
Sampath <kavilashtech@gmail.com>
2023-11-17 11:24:14 +0530

    update textile page and footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,67 +1,70 @@
+<!-- Site footer -->
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-6">
+                <h6>About</h6>
+                <p class="text-justify">ANH Exim is a recently launched export-import firm, driven by a passion for global trade. Our focus is on providing personalized solutions to help businesses expand their reach and source products from around the world. Trust us to be your partner in navigating the world of international trade, where possibilities are endless, and opportunities are boundless. We aim to create value for our clients, partners, and the communities we serve.</p>
+            </div>
 
-    <!-- Footer -->
-    <footer class="text-center text-white">
-        <!-- Grid container -->
-        <div class="container p-4">
+            <div class="col-xs-6 col-md-3">
+                <h6>Categories</h6>
+                <ul class="footer-links">
+                    <li><a href="textile.php">Textile</a></li>
+                    <li><a href="millets.php">Millets</a></li>
+                    <li><a href="moringa.php">Moringa Products</a></li>
+                </ul>
+            </div>
 
-            <!-- Section: Links -->
-            <section class="">
-                <!--Grid row-->
-                <div class="row">
-                    <!--Grid column-->
-                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0 footer-logo">
-                        <!-- Your Company Logo -->
-                        <!-- <a href="#"><img src="./assets/images/LOGO.png" alt="Your Company Logo" width="120"></a> -->
-                        <h3>ANH EXIM</h3>
-                        <p>Excellence in International Trade</p>
-                    </div>
-                    <!--Grid column-->
-
-                    <!--Grid column-->
-                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0 footer-menu">
-                        <h5>Quick Links</h5>
-
-                        <ul class="list-unstyled  mb-0">
-                            <li class="footerlinks"><a href="#">Home</a></li>
-                            <li class="footerlinks"><a href="#">About Us</a></li>
-                            <li class="footerlinks"><a href="#">Service</a></li>
-                            <li class="footerlinks"><a href="#">Contacts</a> </li>
-                        </ul>
-                    </div>
-                    <!--Grid column-->
-
-                    <!--Grid column-->
-                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-
-                    </div>
-                    <!--Grid column-->
-
-                    <!--Grid column-->
-                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-
-                    </div>
-                    <!--Grid column-->
-                </div>
-                <!--Grid row-->
-            </section>
-            <!-- Section: Links -->
+            <div class="col-xs-6 col-md-3">
+                <h6>Quick Links</h6>
+                <ul class="footer-links">
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Terms & Conditions</a></li>
+                </ul>
+            </div>
         </div>
-        <!-- Grid container -->
+        <hr>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-6 col-xs-12">
+                <p class="copyright-text">Copyright &copy; 2023 All Rights Reserved by
+                    ANHEXIM.
+                </p>
+            </div>
 
-        <!-- Copyright -->
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <ul class="social-icons">
+                    <li><a class="facebook" href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                    <li><a class="twitter" href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                    <li><a class="dribbble" href="#"><i class="fa-brands fa-dribbble"></i></a></li>
+                    <li><a class="linkedin" href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- Copyright -->
+    <div class="container mt-3">
         <hr style="margin: 0; border-top: 1px solid white;">
         <div class="text-center footer-bar">
             <span>
-                © 2023 Copyright : ANH Exim&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Design & Development : <a class="text-white" href="#">Kavilash Technologies</a>
+                Design & Development : <a class="text-white" href="#">Kavilash Technologies</a>
             </span>
         </div>
-        <!-- Copyright -->
-    </footer>
-    <!-- Footer -->
+    </div>
+    <!-- Copyright -->
+</footer>
 
 
-    <!-- Copyright Footer (Even Smaller) -->
-    <!-- <footer style="background-color: black; padding: 0;" class="text-white text-center">
+
+
+
+
+<!-- Copyright Footer (Even Smaller) -->
+<!-- <footer style="background-color: black; padding: 0;" class="text-white text-center">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-md-12">
@@ -74,10 +77,10 @@
         </div>
     </footer> -->
 
-    <!-- Include Jquery JS -->
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <!-- Include Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Include Jquery JS -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<!-- Include Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
--- textile.php
+++ textile.php
@@ -1,45 +1,149 @@
 <?php include 'includes/header.php'; ?>
 
+
+<style>
+    .textile-banner img {
+        width: 100%;
+        margin-bottom: 40px;
+    }
+
+    .textile-card {
+        height: 700px;
+    }
+
+    .textile-card h3 {
+        color: var(--dark-brown);
+        font-size: 22px;
+        text-align: center;
+    }
+
+    .textile-card :hover {
+        background: rgba(211, 156, 100, 1);
+    }
+</style>
 <div class="container">
     <img class="head-img" src="assets/images/textilehead.jpg" alt="">
     <h2 class="text-center mb-1 mt-4" style="font-size:36pt">Textiles</h2>
 
     <p class="sub-page-header-text">ANH Exim is a recently launched export-import firm, driven by a passion for global trade. With fresh ideas and a commitment to delivering quality and value, we specialize in facilitating international business connections. Our focus is on providing personalized solutions to help businesses expand their reach and source products from around the world. Trust us to be your partner in navigating the world of international trade, where possibilities are endless, and opportunities are boundless. We aim to create value for our clients, partners, and the communities we serve</p>
 
+    <div class="mt-3">&nbsp;</div>
+    <div class="textile-banner">
+        <img src="assets/images/girl2.jpg" alt="banner" style="object-fit: fill;">
+    </div>
+
+
+
+
+
     <section class="padding-lg">
         <h2 class="text-center mb-5 " style="font-size:30pt;color:#0a193b;">Jeans</h2>
         <div class="container">
-            <ul class="row textile-block">
-                <li class="col-12 col-md-6 col-lg-3">
-                    <div class="cnt-block equal-height" style="height: 349px;">
+
+            <div class="card-group">
+                <div class="card textile-card">
+                    <img src="assets/images/products/jean1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body textile-card-body">
                         <h3>Denim</h3>
-                        <p>Denim is the most common material used in jeans. It is a sturdy cotton twill fabric with a characteristic diagonal ribbing pattern. Denim comes in various weights and finishes, from lightweight to heavyweight, and can be distressed, stonewashed, or left untreated for a raw look.</p>
+                        <p>Denim is the most common material used in jeans. It is a sturdy cotton twill fabric with a characteristic diagonal ribbing pattern. Denim comes in various weights and finishes, from lightweight to heavyweight, and can be distressed, stonewashed, or left untreated for a raw look</p>
                     </div>
-                </li>
-                <li class="col-12 col-md-6 col-lg-3">
-                    <div class="cnt-block equal-height" style="height: 349px;">
+                </div>
+                <div class="card textile-card">
+                    <img src="assets/images/products/jean2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
                         <h3>Stretch Denim</h3>
                         <p>Stretch jeans are made by blending cotton denim with elastane (spandex) to provide a comfortable, flexible fit. This is especially popular in skinny jeans and slim-fit styles.</p>
                     </div>
-                </li>
-                <li class="col-12 col-md-6 col-lg-3">
-                    <div class="cnt-block equal-height" style="height: 349px;">
+                </div>
+                <div class="card textile-card">
+                    <img src="assets/images/products/jean3.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
                         <h3>Selvedge Denim</h3>
                         <p>Selvedge denim is known for its high-quality construction and finished edges. It's often used in premium jeans.</p>
                     </div>
-                </li>
-                <li class="col-12 col-md-6 col-lg-3">
-                    <div class="cnt-block equal-height" style="height: 349px;">
+                </div>
+                <div class="card textile-card">
+                    <img src="assets/images/products/jean4.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
                         <h3>Distressed Denim</h3>
                         <p>Distressed or ripped jeans are intentionally altered to give them a worn and vintage look. They are typically made from traditional denim but undergo various distressing techniques.</p>
                     </div>
-                </li>
-            </ul>
+                </div>
+            </div>
         </div>
     </section>
 
-    <section class="padding-lg">
-        <h2 class="text-center mb-5 mt-5" style="font-size:30pt; color:#0a193b;">T-Shirt</h2>
+
+    <!-- Portfolio Grid Section -->
+        <div class="container mb-5">
+            <h2 class="text-center mb-5 mt-5" style="font-size:30pt;color:#0a193b;">Portfolio</h2>
+            <div class="portfolio-wrapper">
+                <div>
+                    <img src="assets/images/products/textile/textile1.jpg" alt="" />
+                </div>
+                <div>
+                    <img src="assets/images/products/textile/textile2.jpg" alt="" />
+                </div>
+                <div class="tall">
+                    <img src="assets/images/products/textile/textile3.jpg" alt="" />
+                </div>
+                <div class="wide">
+                    <img src="assets/images/products/textile/textile4.jpg" alt="" />
+                </div>
+                <div>
+                    <img src="assets/images/products/textile/textile5.jpg" alt="" />
+                </div>
+                <div>
+                    <img src="assets/images/products/textile/textile6.jpg" alt="" />
+                </div>
+                <div>
+                    <img src="assets/images/products/textile/textile7.jpg" alt="" />
+                </div>
+                <div>
+                    <img src="assets/images/products/textile/textile8.jpg" alt="" />
+                </div>
+                <div class="big">
+                    <img src="assets/images/products/textile/textile9.jpg" alt="" />
+                </div>
+                <div class="tall">
+                    <img src="assets/images/products/textile/textile10.jpg" alt="" />
+                </div>
+                <div class="big">
+                    <img src="assets/images/products/textile/textile11.jpg" alt="" />
+                </div>
+                <div>
+                    <img src="assets/images/products/textile/textile12.jpg" alt="" />
+                </div>
+                <div class="tall">
+                    <img src="assets/images/products/textile/textile13.jpg" alt="" />
+                </div>
+                <div class="big">
+                    <img src="assets/images/products/textile/textile14.jpg" alt="" />
+                </div>
+                <div class="tall">
+                    <img src="assets/images/products/textile/textile15.jpg" alt="" />
+                </div>
+                <div>
+                    <img src="assets/images/products/textile/textile16.jpg" alt="" />
+                </div>
+                <div class="big">
+                    <img src="assets/images/products/textile/textile17.jpg" alt="" />
+                </div>
+                <div class="tall">
+                    <img src="assets/images/products/textile/textile18.jpg" alt="" />
+                </div>
+                <div class="wide">
+                    <img src="assets/images/products/textile/textile19.jpg" alt="" />
+                </div>
+
+            </div>
+        </div>
+
+
+
+
+    <!-- <section class="padding-lg">
+        <h2 class="text-center mb-5 mt-5" style="font-size:30pt; color:#0a193b;">Featured Products</h2>
         <div class="container">
             <ul class="row textile-block">
                 <li class="col-12 col-md-6 col-lg-3">
@@ -68,10 +172,10 @@
                 </li>
             </ul>
         </div>
-    </section>
+    </section> -->
 
     <!-- T-shirt section -->
-    <h2 class="text-center mb-5 mt-5" style="font-size:30pt; color:#0a193b;">Kids Dresses</h2>
+    <!-- <h2 class="text-center mb-5 mt-5" style="font-size:30pt; color:#0a193b;">Kids Dresses</h2>
     <div class="row">
 
         <div class="col-lg-6 col-md-10 kids-block">
@@ -137,14 +241,12 @@
                 </div>
             </div>
         </div>
-    </div>
-<!-- <div class="mt-3">&nbsp;</div>    -->
+    </div> -->
+    <!-- <div class="mt-3">&nbsp;</div>    -->
 
-<section class="padding-lg">
-    <!-- <h2>Gallery</h2> -->
-
-
-</section>
+    <section class="padding-lg">
+        <!-- <h2>Gallery</h2> -->
+    </section>
 </div>
 
 <?php include 'includes/footer.php'; ?>
