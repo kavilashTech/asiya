21a005ab81b79da807a67b66329214a4c9548ecd
Sampath <kavilashtech@gmail.com>
2023-11-24 15:15:42 +0530

    moringa page update

--- moringa.php
+++ moringa.php
@@ -4,13 +4,13 @@
     .social-box .box {
         background: #FFF;
         border-radius: 10px;
-        border:1px solid var(--dark-brown);
+        border: 1px solid var(--dark-brown);
         cursor: pointer;
         margin-top: 10px;
-        margin-bottom:10px;
+        margin-bottom: 10px;
         padding: 40px 10px;
         transition: all 0.5s ease-out;
-        height:80%;
+        height: 80%;
     }
 
     .social-box .box:hover {
@@ -35,6 +35,7 @@
         border-radius: 5px;
         background-color: var(--dark-green);
     }
+
     .social-box h4 {
         color: #fff;
         padding: 3px 0px;
@@ -49,7 +50,84 @@
 
     <p class="sub-page-header-text">At ANH Exim, we are proud to offer premium quality moringa leaves and moringa powder for export. Our products are sourced and processed with the utmost care to ensure they meet the highest standards of nutrition and quality. Whether you're looking to incorporate these nutrient-rich moringa products into your dietary supplements, culinary creations, or wellness offerings, we are your trusted partner for reliable exports. Contact us today to discover the natural goodness of moringa leaves and powder, sourced and prepared to your specifications.</p>
     <p class="sub-page-header-text">Moringa leaves, often referred to as "drumstick leaves" or "malunggay" in various parts of the world, are packed with nutrients and have a long history of culinary and medicinal use. Here is a detailed description of moringa leaves and their uses:</p>
+    <section>
+        <div class="container">
+            <div class="row no-gutters block-content">
+                <!-- <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-center"> -->
+                <div class="col-md-4 text-center">
+                    <!--img-->
+                    <img src="assets/images/products/moringa1.jpg" alt="demo-img" class="img-moringa img-fluid">
+                </div>
+                <!-- <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6"> -->
+                <div class="col-md-8">
+                    <!--text-->
+                    <div class="jumbotron jumbotron-fluid text-moringa">
+                        <div class="container">
+                            <h3>Moringa Leaves</h3>
+                            <p>Moringa leaves are small to medium-sized green leaves that are typically composed of leaflets with a feather-like arrangement.
+                                Moringa leaves are highly nutritious, containing a wealth of essential vitamins and minerals.</p>
+                            <p> They are a rich source of vitamin C, vitamin A, vitamin K, calcium, potassium, and iron.
+                                They are also a good source of plant-based protein, making them an attractive option for vegetarians and vegans.</p>
 
+                            <p>Rich in antioxidants, including quercetin, chlorogenic acid, and beta-carotene, moringa helps in combating oxidative stress and reduce inflammation in the body.</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row no-gutters block-content">
+                <!-- <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6"> -->
+                <div class="col-md-8">
+                    <!--text-->
+                    <div class="jumbotron jumbotron-fluid text-moringa">
+                        <div class="container">
+                            <h3>Medicinal Uses</h3>
+                            <p>The bark, sap, roots, leaves, seeds, and flowers are used in traditional medicine.</p>
+                            <p>In developing countries, moringa has the potential to improve nutrition and have been used to combat malnutrition, especially among infants and nursing mothers.</p>
+                            <p>Moringa leaves are anti-inflammatory in nature due to the presence of isothiocyanates. They have niazimicin that is known to reign in the development of cancer cells. Inflammation is the root cause of many diseases like cancer, arthritis, rheumatoid arthritis, and many autoimmune diseases. When we suffer an injury or infection, the body suffers increased inflammation.</p>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-center"> -->
+                <div class="col-md-4 text-center">
+                    <!--img-->
+                    <img src="assets/images/products/moringa2.jpg" alt="demo-img" class="img-moringa img-fluid">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row no-gutters block-content">
+                <!-- <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 text-center"> -->
+                <div class="col-md-4 text-center">
+                    <!--img-->
+                    <img src="assets/images/products/moringa3.jpg" alt="demo-img" class="img-moringa img-fluid">
+                </div>
+                <!-- <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6"> -->
+                <div class="col-md-8">
+                    <!--text-->
+                    <div class="jumbotron jumbotron-fluid text-moringa">
+                        <div class="container">
+                            <h3>Other Uses</h3>
+                            <p><strong>Culinary :</strong> Moringa leaves are commonly used in various culinary dishes in countries where the moringa tree is grown. They can be added to soups, stews, curries, and salads.</p>
+                            <p>Moringa leaves are often used to make a nutritious, green leafy vegetable side dish.</p>
+                            <p>They can also be dried and powdered to make moringa powder, which is used as a nutritional supplement or as a seasoning in various dishes.</p>
+                            
+                            <p><strong>Cosmetics :</strong>leaves can be used to create natural skincare products due to their anti-inflammatory and antioxidant properties. They are used in masks, creams, and lotions.</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--     
     <div class="social-box">
         <div class="container">
             <div class="row">
@@ -104,9 +182,9 @@
 
             </div>
         </div>
-    </div>
+    </div> -->
 
-    <h4 style="text-align: center;margin-top:50px;font-weight:bold;color:var(--dark-green);">Medicinal Uses</h4>
+    <!-- <h4 style="text-align: center;margin-top:50px;font-weight:bold;color:var(--dark-green);">Medicinal Uses</h4>
 
     <div class="social-box">
         <div class="container">
@@ -184,16 +262,16 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 
 
 
-    <p class="sub-page-header-text"><strong>Culinary Uses:</strong>
-Moringa leaves are commonly used in various culinary dishes in countries where the moringa tree is grown. They can be added to soups, stews, curries, and salads.
-Moringa leaves are often used to make a nutritious, green leafy vegetable side dish.<br><br>
-They can also be dried and powdered to make moringa powder, which is used as a nutritional supplement or as a seasoning in various dishes.
-</p>
-    <p class="sub-page-header-text"><strong>Natural Skincare:</strong> leaves can be used to create natural skincare products due to their anti-inflammatory and antioxidant properties. They are used in masks, creams, and lotions.</p>
+    <!-- <p class="sub-page-header-text"><strong>Culinary Uses:</strong>
+        Moringa leaves are commonly used in various culinary dishes in countries where the moringa tree is grown. They can be added to soups, stews, curries, and salads.
+        Moringa leaves are often used to make a nutritious, green leafy vegetable side dish.<br><br>
+        They can also be dried and powdered to make moringa powder, which is used as a nutritional supplement or as a seasoning in various dishes.
+    </p>
+    <p class="sub-page-header-text"><strong>Natural Skincare:</strong> leaves can be used to create natural skincare products due to their anti-inflammatory and antioxidant properties. They are used in masks, creams, and lotions.</p> -->
 
 </div>
 
